0a54eba9e29bfd3b3f31efb1b44ab6591a5d3966
Diego Vasconcelos <diegovasconcelosb@gmail.com>
2024-12-03 10:32:35 -0300

    chore: comment out Usuario section in Program.cs and start implementing Livro-specific logic

--- Program.cs
+++ Program.cs
@@ -2,19 +2,163 @@ using Biblioteca.Entidades;
 
 class Program
 {
+//     static async Task Main(string[] args)
+//     {
+//         var connectionString = "server=localhost;port=3307;database=app_db;user=app_user;password=user123;";
+//         var usuarioRepository = new UsuarioRepository(connectionString);
+//         var usuarioService = new UsuarioService(usuarioRepository);
+
+//         while (true)
+//         {
+//             Console.WriteLine("1. Adicionar Usuário");
+//             Console.WriteLine("2. Buscar Usuário por ID");
+//             Console.WriteLine("3. Listar Todos os Usuários");
+//             Console.WriteLine("4. Atualizar Usuário");
+//             Console.WriteLine("5. Deletar Usuário");
+//             Console.WriteLine("0. Sair");
+//             Console.Write("Escolha uma opção: ");
+
+//             var opcao = Console.ReadLine();
+//             switch (opcao)
+//             {
+//                 case "1":
+//                     await AdicionarUsuario(usuarioService);
+//                     continue;
+//                 case "2":
+//                     await BuscarUsuario(usuarioService);
+//                     continue;
+//                 case "3":
+//                     await BuscarTodosUsuarios(usuarioService);
+//                     continue;
+//                 case "4":
+//                     await AtualizarUsuario(usuarioService);
+//                     continue;
+//                 case "5": 
+//                     await DeletarUsuario(usuarioService);
+//                     continue;
+//                 case "0":
+//                     return;
+//                 default:
+//                     Console.WriteLine("Opção inválida.");
+//                     return;
+//             }
+//         }
+//     }
+
+//     static async Task AdicionarUsuario(UsuarioService usuarioService)
+//     {
+//         Console.Write("Nome: ");
+//         var nome = Console.ReadLine();
+
+//         Console.Write("Idade: ");
+//         var idade = int.Parse(Console.ReadLine());
+
+//         Console.WriteLine("Data de Nascimento (yyyy-MM-dd): ");
+//         var DataDeNascimento = DateTime.Parse(Console.ReadLine());
+
+//         Console.WriteLine("CPF: ");
+//         var cpf = Console.ReadLine();
+
+//         Console.WriteLine("Telefone: ");
+//         var telefone = Console.ReadLine();
+        
+//         Console.WriteLine("Email: ");
+//         var email = Console.ReadLine();
+
+//         var usuario = new Usuario
+//         {
+//             Nome = nome,
+//             Idade = idade,
+//             DataDeNascimento = DataDeNascimento,
+//             Cpf = cpf,
+//             Telefone = telefone,
+//             Email = email
+//         };
+
+//         var resultado = await usuarioService.AdicionarUsuario(usuario);
+//         Console.WriteLine($"Usuario adicionado com ID: {resultado.Id}");
+//     }
+
+//     static async Task BuscarUsuario(UsuarioService usuarioService)
+//     {
+//         Console.WriteLine("Digite o ID do Usuário: ");
+//         var id = int.Parse(Console.ReadLine());
+//         var usuario = await usuarioService.BuscarUsuario(id);
+
+//         Console.WriteLine($"ID: {usuario.Id}, Nome: {usuario.Nome}, Idade: {usuario.Idade}, Data de Nascimento: {usuario.DataDeNascimento}, CPF: {usuario.Cpf}, Telefone: {usuario.Telefone}, Email: {usuario.Email}");        
+//     }
+
+//     static async Task BuscarTodosUsuarios(UsuarioService usuarioService)
+//     {
+//         var usuarios = await usuarioService.BuscarTodosUsuarios();
+
+//         foreach (var usuario in usuarios)
+//         {
+//             Console.WriteLine($"ID: {usuario.Id}, Nome: {usuario.Nome}, Idade: {usuario.Idade}, Data de Nascimento: {usuario.DataDeNascimento}, CPF: {usuario.Cpf}, Telefone: {usuario.Telefone}, Email: {usuario.Email}");
+//         }
+//     }
+
+//     static async Task AtualizarUsuario(UsuarioService usuarioService)
+//     {
+//         Console.Write("Digite o ID do usuário a ser atualizado: ");
+//         var id = int.Parse(Console.ReadLine());
+
+//         var usuarioExistente = await usuarioService.BuscarUsuario(id);
+//         if (usuarioExistente == null)
+//         {
+//             Console.WriteLine("Usuário não encontrado.");
+//             return;
+//         }
+
+//         Console.Write("Novo Nome (deixe em branco para não alterar): ");
+//         var nome = Console.ReadLine();
+//         if (!string.IsNullOrWhiteSpace(nome)) usuarioExistente.Nome = nome;
+
+//         Console.Write("Nova Idade (deixa em branco para não alterar): ");
+//         var idade = Console.ReadLine();
+//         if (!string.IsNullOrWhiteSpace(idade)) usuarioExistente.Idade = int.Parse(idade);
+
+//         Console.Write("Nova Data de Nascimento (deixe em branco para não alterar): ");
+//         var data = Console.ReadLine();
+//         if (!string.IsNullOrWhiteSpace(data)) usuarioExistente.DataDeNascimento = DateTime.Parse(data);
+
+//         Console.Write("Novo Cpf (deixe em branco para não alterar): ");
+//         var cpf = Console.ReadLine();
+//         if (!string.IsNullOrWhiteSpace(cpf)) usuarioExistente.Cpf = cpf;
+
+//         Console.Write("Novo Telefone (deixe em branco para não alterar): ");
+//         var telefone = Console.ReadLine();
+//         if (!string.IsNullOrWhiteSpace(telefone)) usuarioExistente.Telefone = telefone;
+        
+//         Console.Write("Novo Email (Deixe em branco para não alterar): ");
+//         var email = Console.ReadLine();
+//         if (!string.IsNullOrWhiteSpace(email)) usuarioExistente.Email = email;
+
+//         await usuarioService.AtualizarUsuario(usuarioExistente);
+//         Console.WriteLine("Usuário atualizado com sucesso.");
+//     }
+
+//     static async Task DeletarUsuario(UsuarioService usuarioService)
+//     {
+//         Console.Write("Digite o ID do usuário a ser detalhado: ");
+//         var id = int.Parse(Console.ReadLine());
+
+//         var sucesso = await usuarioService.DeletarUsuario(id);
+//         Console.WriteLine(sucesso ? "Usuário deletado com sucesso." : "Erro ao deletar usuário.");
+//     }
     static async Task Main(string[] args)
     {
         var connectionString = "server=localhost;port=3307;database=app_db;user=app_user;password=user123;";
-        var usuarioRepository = new UsuarioRepository(connectionString);
-        var usuarioService = new UsuarioService(usuarioRepository);
+        var livroRepository = new LivroRepository(connectionString);
+        var livroService = new LivroService(livroRepository);
 
         while (true)
         {
-            Console.WriteLine("1. Adicionar Usuário");
-            Console.WriteLine("2. Buscar Usuário por ID");
-            Console.WriteLine("3. Listar Todos os Usuários");
-            Console.WriteLine("4. Atualizar Usuário");
-            Console.WriteLine("5. Deletar Usuário");
+            Console.WriteLine("1. Adicionar Livro");
+            Console.WriteLine("2. Buscar Livro por ID");
+            Console.WriteLine("3. Listar Todos os Livros");
+            Console.WriteLine("4. Atualizar Livro");
+            Console.WriteLine("5. Deletar Livro");
             Console.WriteLine("0. Sair");
             Console.Write("Escolha uma opção: ");
 
@@ -22,128 +166,121 @@ class Program
             switch (opcao)
             {
                 case "1":
-                    await AdicionarUsuario(usuarioService);
+                    await AdicionarLivro(livroService);
                     continue;
                 case "2":
-                    await BuscarUsuario(usuarioService);
+                    await BuscarLivro(livroService);
                     continue;
                 case "3":
-                    await BuscarTodosUsuarios(usuarioService);
+                    await BuscarTodosLivros(livroService);
                     continue;
                 case "4":
-                    await AtualizarUsuario(usuarioService);
+                    await AtualizarLivro(livroService);
                     continue;
-                case "5": 
-                    await DeletarUsuario(usuarioService);
+                case "5":
+                    await DeletarLivro(livroService);
                     continue;
                 case "0":
                     return;
                 default:
                     Console.WriteLine("Opção inválida.");
-                    return;
+                    continue;
             }
         }
     }
 
-    static async Task AdicionarUsuario(UsuarioService usuarioService)
+    static async Task AdicionarLivro(LivroService livroService)
     {
-        Console.Write("Nome: ");
-        var nome = Console.ReadLine();
+        Console.Write("Título: ");
+        var titulo = Console.ReadLine();
 
-        Console.Write("Idade: ");
-        var idade = int.Parse(Console.ReadLine());
+        Console.Write("Gênero: ");
+        var genero = Console.ReadLine();
 
-        Console.WriteLine("Data de Nascimento (yyyy-MM-dd): ");
-        var DataDeNascimento = DateTime.Parse(Console.ReadLine());
+        Console.Write("Autor: ");
+        var autor = Console.ReadLine();
 
-        Console.WriteLine("CPF: ");
-        var cpf = Console.ReadLine();
+        Console.Write("Número de Páginas: ");
+        var numeroDePaginas = int.Parse(Console.ReadLine());
 
-        Console.WriteLine("Telefone: ");
-        var telefone = Console.ReadLine();
-        
-        Console.WriteLine("Email: ");
-        var email = Console.ReadLine();
+        Console.Write("Ano de Publicação: ");
+        var anoPublicacao = int.Parse(Console.ReadLine());
 
-        var usuario = new Usuario
+        var livro = new Livro
         {
-            Nome = nome,
-            Idade = idade,
-            DataDeNascimento = DataDeNascimento,
-            Cpf = cpf,
-            Telefone = telefone,
-            Email = email
+            Titulo = titulo,
+            Genero = genero,
+            Autor = autor,
+            NumeroDePaginas = numeroDePaginas,
+            AnoPublicacao = anoPublicacao
         };
 
-        var resultado = await usuarioService.AdicionarUsuario(usuario);
-        Console.WriteLine($"Usuario adicionado com ID: {resultado.Id}");
+        var resultado = await livroService.AdicionarLivro(livro);
+        Console.WriteLine($"Livro adicionado com ID: {resultado.Id}");
     }
 
-    static async Task BuscarUsuario(UsuarioService usuarioService)
+    static async Task BuscarLivro(LivroService livroService)
     {
-        Console.WriteLine("Digite o ID do Usuário: ");
+        Console.Write("Digite o ID do livro: ");
         var id = int.Parse(Console.ReadLine());
-        var usuario = await usuarioService.BuscarUsuario(id);
+        var livro = await livroService.BuscarLivro(id);
 
-        Console.WriteLine($"ID: {usuario.Id}, Nome: {usuario.Nome}, Idade: {usuario.Idade}, Data de Nascimento: {usuario.DataDeNascimento}, CPF: {usuario.Cpf}, Telefone: {usuario.Telefone}, Email: {usuario.Email}");        
+        Console.WriteLine($"ID: {livro.Id}, Título: {livro.Titulo}, Gênero: {livro.Genero}, Autor: {livro.Autor}, Páginas: {livro.NumeroDePaginas}, Ano: {livro.AnoPublicacao}");
     }
 
-    static async Task BuscarTodosUsuarios(UsuarioService usuarioService)
+    static async Task BuscarTodosLivros(LivroService livroService)
     {
-        var usuarios = await usuarioService.BuscarTodosUsuarios();
+        var livros = await livroService.BuscarTodosLivros();
 
-        foreach (var usuario in usuarios)
+        foreach (var livro in livros)
         {
-            Console.WriteLine($"ID: {usuario.Id}, Nome: {usuario.Nome}, Idade: {usuario.Idade}, Data de Nascimento: {usuario.DataDeNascimento}, CPF: {usuario.Cpf}, Telefone: {usuario.Telefone}, Email: {usuario.Email}");
+            Console.WriteLine($"ID: {livro.Id}, Título: {livro.Titulo}, Gênero: {livro.Genero}, Autor: {livro.Autor}, Páginas: {livro.NumeroDePaginas}, Ano: {livro.AnoPublicacao}");
         }
     }
 
-    static async Task AtualizarUsuario(UsuarioService usuarioService)
+    static async Task AtualizarLivro(LivroService livroService)
     {
-        Console.Write("Digite o ID do usuário a ser atualizado: ");
+        Console.Write("Digite o ID do livro a ser atualizado: ");
         var id = int.Parse(Console.ReadLine());
 
-        var usuarioExistente = await usuarioService.BuscarUsuario(id);
-        if (usuarioExistente == null)
+        var livroExistente = await livroService.BuscarLivro(id);
+        if (livroExistente == null)
         {
-            Console.WriteLine("Usuário não encontrado.");
+            Console.WriteLine("Livro não encontrado.");
             return;
         }
 
-        Console.Write("Novo Nome (deixe em branco para não alterar): ");
-        var nome = Console.ReadLine();
-        if (!string.IsNullOrWhiteSpace(nome)) usuarioExistente.Nome = nome;
+        Console.Write("Novo Título (deixe em branco para não alterar): ");
+        var titulo = Console.ReadLine();
+        if (!string.IsNullOrWhiteSpace(titulo)) livroExistente.Titulo = titulo;
 
-        Console.Write("Nova Idade (deixa em branco para não alterar): ");
-        var idade = Console.ReadLine();
-        if (!string.IsNullOrWhiteSpace(idade)) usuarioExistente.Idade = int.Parse(idade);
+        Console.Write("Novo Gênero (deixe em branco para não alterar): ");
+        var genero = Console.ReadLine();
+        if (!string.IsNullOrWhiteSpace(genero)) livroExistente.Genero = genero;
 
-        Console.Write("Nova Data de Nascimento (deixe em branco para não alterar): ");
-        var data = Console.ReadLine();
-        if (!string.IsNullOrWhiteSpace(data)) usuarioExistente.DataDeNascimento = DateTime.Parse(data);
+        Console.Write("Novo Autor (deixe em branco para não alterar): ");
+        var autor = Console.ReadLine();
+        if (!string.IsNullOrWhiteSpace(autor)) livroExistente.Autor = autor;
 
-        Console.Write("Novo Cpf (deixe em branco para não alterar): ");
-        var cpf = Console.ReadLine();
-        if (!string.IsNullOrWhiteSpace(cpf)) usuarioExistente.Cpf = cpf;
+        Console.Write("Novo Número de Páginas (deixe em branco para não alterar): ");
+        var paginas = Console.ReadLine();
+        if (!string.IsNullOrWhiteSpace(paginas)) livroExistente.NumeroDePaginas = int.Parse(paginas);
 
-        Console.Write("Novo Telefone (deixe em branco para não alterar): ");
-        var telefone = Console.ReadLine();
-        if (!string.IsNullOrWhiteSpace(telefone)) usuarioExistente.Telefone = telefone;
-        
-        Console.Write("Novo Email (Deixe em branco para não alterar): ");
-        var email = Console.ReadLine();
-        if (!string.IsNullOrWhiteSpace(email)) usuarioExistente.Email = email;
+        Console.Write("Novo Ano de Publicação (deixe em branco para não alterar): ");
+        var ano = Console.ReadLine();
+        if (!string.IsNullOrWhiteSpace(ano)) livroExistente.AnoPublicacao = int.Parse(ano);
 
-        await usuarioService.AtualizarUsuario(usuarioExistente);
-        Console.WriteLine("Usuário atualizado com sucesso.");
+        await livroService.AtualizarLivro(livroExistente);
+        Console.WriteLine("Livro atualizado com sucesso.");
     }
 
-    static async Task DeletarUsuario(UsuarioService usuarioService)
+    static async Task DeletarLivro(LivroService livroService)
     {
-        Console.Write("Digite o ID do usuário a ser detalhado: ");
+        Console.Write("Digite o ID do livro a ser deletado: ");
         var id = int.Parse(Console.ReadLine());
 
-        var sucesso = await usuarioService.DeletarUsuario(id);
-        Console.WriteLine(sucesso ? "Usuário deletado com sucesso." : "Erro ao deletar usuário.");
+        var sucesso = await livroService.DeletarLivro(id);
+        Console.WriteLine(sucesso ? "Livro deletado com sucesso" : "Erro ao deletar livro");
     }
+
 }
