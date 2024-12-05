e16bd78ef351949e2e223bb70c6076b52b8efe38
Diego Vasconcelos <diegovasconcelosb@gmail.com>
2024-12-05 17:04:02 -0300

    feat: add method BuscarTodosInventarios to IInventarioRepository interface

--- Interfaces/IInventarioRepository.cs
+++ Interfaces/IInventarioRepository.cs
@@ -4,6 +4,7 @@ public interface IInventarioRepository
 {
     Task<Inventario> AdicionarInventario(Inventario inventario);
     Task<Inventario> BuscarInventario(int id);
+    Task<IEnumerable<Inventario>> BuscarTodosInventarios();
     Task<Inventario> AtualizarInventario(Inventario inventario);
     Task<bool> DeletarInventario(int id);
 }
