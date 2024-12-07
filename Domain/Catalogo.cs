d6cf3a2c185a2a1df286e1c11b2661e9cf9fa9cd
Diego Vasconcelos <diegovasconcelosb@gmail.com>
2024-12-07 18:13:12 -0300

    refactor: update Catalogo entity attributes and constructor, commented out validations

--- Domain/Catalogo.cs
+++ Domain/Catalogo.cs
@@ -4,24 +4,24 @@ namespace Biblioteca.Entidades
     {
         public int Id {get; set;}
         public string Nome {get; set;} = string.Empty;
-        public int InventarioId {get; set;}
         public string Genero {get; set;} = string.Empty;
+        public List<Livro> Livros {get; set;}
 
-        public Catalogo(int id, string nome, int inventarioId, string genero)
+        public Catalogo(int id, string nome, string genero)
         {
             Id = id;
             Nome = nome;
-            InventarioId = inventarioId;
             Genero = genero;
+            Livros = new List<Livro>();
 
-            if (string.IsNullOrWhiteSpace(nome))
-                throw new ArgumentException("Nome do catálogo não pode ser vazio.");
+            // if (string.IsNullOrWhiteSpace(nome))
+            //     throw new ArgumentException("Nome do catálogo não pode ser vazio.");
 
-            if (inventarioId <= 0)
-                throw new ArgumentException("Livro do catálogo não pode ser zero ou negativo.");
+            // if (livroId <= 0)
+            //     throw new ArgumentException("Livro do catálogo não pode ser zero ou negativo.");
 
-            if (string.IsNullOrWhiteSpace(genero))
-                throw new ArgumentException("Gênero do catálogo não pode ser vazio.");
+            // if (string.IsNullOrWhiteSpace(genero))
+            //     throw new ArgumentException("Gênero do catálogo não pode ser vazio.");
         }
     }
 }    
