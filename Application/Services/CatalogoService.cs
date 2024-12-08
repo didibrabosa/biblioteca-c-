fb41afb80faf5f7583b9338cedd7bb836f3c9411
Diego Vasconcelos <diegovasconcelosb@gmail.com>
2024-12-08 00:28:53 -0300

    feat: implement CatalogoService in the Services layer

---
+++ Application/Services/CatalogoService.cs
@@ -0,0 +1,40 @@
+using Biblioteca.Entidades;
+
+public class CatalogoService
+{
+    private readonly ICatalogoRepository _catalogoRepository;
+
+    public CatalogoService(ICatalogoRepository catalogoRepository)
+    {
+        _catalogoRepository = catalogoRepository;
+    }
+
+    public async Task<Catalogo> AdicionarCatalogo(Catalogo catalogo, int livroId)
+        => catalogo != null ? await _catalogoRepository.AdicionarCatalogo(catalogo, livroId) : null;
+    
+    public async Task<Catalogo> BuscarCatalogo(int id)
+        => id > 0 ? await _catalogoRepository.BuscarCatalogo(id) : null;
+
+    public async Task<IEnumerable<Catalogo>> BuscarTodosCatalogos()
+        => await _catalogoRepository.BuscarTodosCatalogos();
+
+    public async Task<bool> DeletarCatalogo(int id)
+        => id > 0 && await _catalogoRepository.DeletarCatalogo(id);
+
+    public async Task AdicionarLivroCatalogo(int catalogoId, int livroId)
+    {
+        if (catalogoId > 0 && livroId > 0)
+        {
+            await _catalogoRepository.AdicionarLivroCatalogo(catalogoId, livroId);
+        }
+    }
+
+    public async Task RemoverLivroCatalogo(int catalogoId, int livroId)
+    {
+        if (catalogoId > 0 && livroId > 0)
+        {
+            await _catalogoRepository.RemoverLivroCatalogo(catalogoId, livroId);
+        }
+    }
+    
+}
