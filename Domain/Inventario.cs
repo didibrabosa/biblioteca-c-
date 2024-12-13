c1d0f0535fbb2642d94137e344b4c4cebcc8d1d5
Diego Vasconcelos <diegovasconcelosb@gmail.com>
2024-12-13 18:54:42 -0300

    feat: add 'Disponibilidade' attribute to Inventario entity to support Emprestimo implementation

--- Domain/Inventario.cs
+++ Domain/Inventario.cs
@@ -6,15 +6,17 @@ namespace Biblioteca.Entidades
         public int LivroId {get; set;}
         public int Quantidade {get; set;} = int.MaxValue;
         public string Estado {get; set;} = string.Empty;
+        public  bool Disponibilidade {get; set;} = true;
 
         public Inventario() { }
         
-        public Inventario(int id, int livroId, int quantidade, string estado)
+        public Inventario(int id, int livroId, int quantidade, string estado, bool disponibilidade)
         {
             Id = id;
             LivroId = livroId;
             Quantidade = quantidade;
-            Estado = estado;     
+            Estado = estado;
+            Disponibilidade = disponibilidade;
         }
     }
 }
